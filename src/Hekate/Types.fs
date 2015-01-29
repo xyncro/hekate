9f03cd199fb10f695e4a41a57b3425338f313ed3
kolektiv <andrew@xyncro.com>
2015-01-29 17:40:33 +0000

    initial projs

---
+++ src/Hekate/Types.fs
@@ -0,0 +1,12 @@
+﻿[<AutoOpen>]
+module Hekate.Types
+
+type Graph<'v,'e> =
+    { Vertices: Vertex<'v> list
+      Edges: Edge<'e> list }
+
+and Vertex<'v> =
+    { Data: 'v }
+
+and Edge<'e> =
+    { Data: 'e }
