bab2e04a5074216e16ed2d7358a0135403d99ba2
kolektiv <andrew@xyncro.com>
2015-06-05 15:24:19 +0100

    1.0

--- build.fsx
+++ build.fsx
@@ -29,7 +29,7 @@ Target "Publish" (fun _ ->
               Project = "Hekate"
               OutputPath = tempDir
               WorkingDir = srcDir
-              Version = "0.4.0"
+              Version = "1.0.0"
               AccessKey = getBuildParamOrDefault "nuget_key" ""
               Publish = hasBuildParam "nuget_key"
               Dependencies =
